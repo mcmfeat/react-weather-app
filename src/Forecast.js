2890e312c1b42918b2b3d2f3670c537e538f2003
mcmfeat <marine.miranda@hotmail.com>
2022-11-30 17:06:06 +0000

    Forecast complete

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,70 +1,42 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import axios from "axios";
 
 import "./Forecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  const apiKey = "4e98dbaf0a9o9f25a430tc802924d6f0";
-  let city = props.data.city;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-sm-3 icon">
-            <span className="Forecast-icon">
-              <WeatherIcon icon={props.data.icon} size={20} />
-            </span>
-          </div>
-          <div className="col-sm-3 Forecast-day">Mon.</div>
-          <div className="col-sm-3 Forecast-sky">Clear</div>
-          <div className="col-sm-3 Forecast-tempMaxMin">30º/14º</div>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div>
+          {forecast.map((dailyForecast, index) => {
+            if (index < 5) {
+              return (
+                <div key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
         <hr />
-        {/*         <div className="row">
-          <div className="col-sm-3 icon">
-            <span className="material-symbols-outlined">cloudy</span>
-          </div>
-          <div className="col-sm-3 day">Tue.</div>
-          <div className="col-sm-3 sky">Clouds</div>
-          <div className="col-sm-3 tempMaxMin">33º/13º</div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-sm-3 icon">
-            <span className="material-symbols-outlined">sunny</span>
-          </div>
-          <div className="col-sm-3 day">Wed.</div>
-          <div className="col-sm-3 sky">Clear</div>
-          <div className="col-sm-3 tempMaxMin">38º/13º</div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-sm-3 icon">
-            <span className="material-symbols-outlined">air</span>
-          </div>
-          <div className="col-sm-3 day">Thu.</div>
-          <div className="col-sm-3 sky">Windy</div>
-          <div className="col-sm-3 tempMaxMin">33º/10º</div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-sm-3 icon">
-            <span className="material-symbols-outlined">partly_cloudy_day</span>
-          </div>
-          <div className="col-sm-3 day">Fri.</div>
-          <div className="col-sm-3 sky">Partly cloudy</div>
-          <div className="col-sm-3 tempMaxMin">33º/14º</div>
-        </div> */}
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "4e98dbaf0a9o9f25a430tc802924d6f0";
+    let city = props.data.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
